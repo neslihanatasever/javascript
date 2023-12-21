1914dc522a6937e863e9a80cd463c7b3b4e00d1a
neslihan <neslihan_atasvr111@outlook.com>
2023-12-22 01:40:50 +0300

    commit

---
+++ function.js
@@ -0,0 +1,15 @@
+//fonksiyonlar
+
+function firstFunc(){
+    console.log("nesli");
+}
+firstFunc(); //fonksiyonu çağırdık
+
+
+const firstFunc = () => {
+    //arrow function, yukarıda yazılandan sadece syntax farkı var.
+}
+
+let kullanici = {
+    
+}
---
+++ objectandarrays.js
@@ -0,0 +1,26 @@
+//giriş seviyesinde obje ve array tanımlama
+
+let name = "kadir"
+let surname = "ildeniz"
+let age = 25;
+
+//baslangıc seviye obje ve değişken tanımlama 
+
+let info = { //info objesi içerisinde değişkenlerimizi tutabiliyoruz.
+    name: "kadir",
+    surname: "ildeniz",
+    age: 25
+}
+
+let arr =["1", "2" , "3" , "4" , "5"] //array içerisinde değişkenlerimizi tutabiliyoruz.
+let arr1 = [1,2,3,4,5] //bu sekilde de tutabiliriz
+
+let arr2 = [ //dizi içinde hem string hem number değişkenlerimi yazdırabilirim.
+    {
+        name: "kadir",
+        surname: "ildeniz",
+        age: 25
+    }
+]
+
+console.log(arr[0].name)
