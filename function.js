7e3b143f1174fd987bdfff2c7f04533ca6547db9
neslihan <neslihan_atasvr111@outlook.com>
2023-12-27 19:04:39 +0300

    Update function.js

--- function.js
+++ function.js
@@ -1,15 +1,40 @@
 //fonksiyonlar
 
-function firstFunc(){
-    console.log("nesli");
-}
-firstFunc(); //fonksiyonu çağırdık
+// function firstFunc(){
+//     console.log("nesli");
+// }
+// firstFunc(); //fonksiyonu çağırdık
 
 
-const firstFunc = () => {
-    //arrow function, yukarıda yazılandan sadece syntax farkı var.
+// const firstFunc = () => {
+//     //arrow function, yukarıda yazılandan sadece syntax farkı var.
+// }
+
+// let kullanici = {}
+
+//-------------------------------------------
+
+// let sayi1 = 20;
+// let sayi2 =10; 
+
+// const sum  = (num1, num2) => {
+//     let result = num1 + num2; bu bana sonuc döndürmek çünkü return etmedik.
+//     return result;
+// }
+
+// let res = sum(sayi1,sayi2)
+// console.log(res)
+
+//-------------------------------------------
+
+//call, apply, bind
+
+function hello() {
+    //this 
+    console.log(this.name + this.val) 
 }
 
-let kullanici = {
-    
-}
+let obj = {name: "js", val:"20"}
+
+hello.call(obj) 
+
