468ffd210bd9ba1f7c3dcf81438f85da2298ffe6
neslihan <neslihan_atasvr111@outlook.com>
2024-02-14 01:57:50 +0300

    add butonu calısıyorrr

--- miniprojects/taskapp/script.js
+++ miniprojects/taskapp/script.js
@@ -2,11 +2,11 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
 function addTask() {
-  if (inputBox.value === "") {
-    alert("Please enter a task");
+  if (inputBox.value === "") { //inputBox değişkeni boş mu diye kontrol ettim
+    alert("Please enter a task"); //eğer bu değişkenim boş ise kullanıcıya uyarı verdim
   } else {
-    let li = document.createElement("li");
-    li.innerHTML = inputBox.value;
-    listContainer.appendChild(li);
+    let li = document.createElement("li"); //li etiketini olusturdum ve bu elementi bir li değişkenine atadım 
+    li.innerHTML = inputBox.value; //oluşturduğum li elementinin içerği inputboxın değeri ile doldurdum. inputboxın değerli kullanıcının girdiği değer oluyor.
+    listContainer.appendChild(li);  //oluşturduğum li elementini listContainer elementine ekledim
   }
 }
